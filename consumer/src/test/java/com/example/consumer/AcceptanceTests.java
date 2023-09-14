2edb635ea56dc7d40a1b3e6e8adcac898ba07808
Marco Garofalo <mgarofalo@vmware.com>
2023-09-14 19:00:51 +0100

    Remove unnecessary code

--- consumer/src/test/java/com/example/consumer/AcceptanceTests.java
+++ consumer/src/test/java/com/example/consumer/AcceptanceTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -18,8 +17,6 @@ import org.springframework.test.context.DynamicPropertySource;
     webEnvironment = RANDOM_PORT
 )
 class AcceptanceTests {
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate client;
@@ -34,7 +31,7 @@ class AcceptanceTests {
 
     @Test
     void greetingsAreEncoded() {
-        var greeting = client.getForObject("http://localhost:%s/".formatted(port), String.class);
+        var greeting = client.getForObject("/", String.class);
         assertThatJson(greeting).isEqualTo("""
 		{
 			"encoded": "SGVsbG8gV29ybGQh",
